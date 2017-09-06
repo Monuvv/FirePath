a2b71b0fd0d5f21e4db2bf1d8c4cfa7090cc17f7
Monuvv <monikav306@gmail.com>
2017-09-06 17:24:43 +0530

    its fine

---
+++ FirebaseExample/app/src/main/java/com/example/monikaverma/firebaseexample/Config.java
@@ -0,0 +1,14 @@
+package com.example.monikaverma.firebaseexample;
+
+/**
+ * Created by Monika.verma on 21-07-2017.
+ */
+
+public class Config {
+
+public static final String FIREBASE_URL = "https://fir-demo-8f349.firebaseio.com/";
+
+
+}
+
+
